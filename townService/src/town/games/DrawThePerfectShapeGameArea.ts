2acecb0f40b1913c57de2663e7c8270ad1c375cc
domimo22 <imondi.d@northeastern.edu>
2023-10-31 12:45:11 -0400

    added file for drawtheperfectshapegamearea and added interactable type for the area

---
+++ townService/src/town/games/DrawThePerfectShapeGameArea.ts
@@ -0,0 +1,20 @@
+import Player from '../../lib/Player';
+import {
+  InteractableCommand,
+  InteractableCommandReturnType,
+  InteractableType,
+} from '../../types/CoveyTownSocket';
+import GameArea from './GameArea';
+
+export default class DrawThePerfectShapeGameArea extends GameArea<DrawThePerfectShapeGame> {
+  public handleCommand<CommandType extends InteractableCommand>(
+    command: CommandType,
+    player: Player,
+  ): InteractableCommandReturnType<CommandType> {
+    throw new Error('Method not implemented.');
+  }
+
+  protected getType(): InteractableType {
+    return 'DrawThePerfectShapeArea';
+  }
+}
