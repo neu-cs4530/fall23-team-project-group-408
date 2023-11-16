2e456166af2ed201e8fcb5cc984423def168d50b
domimo22 <imondi.d@northeastern.edu>
2023-11-16 16:05:22 -0500

    added basic connection for firestore database and method to add data to the collection

---
+++ townService/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export default {
+    apiKey: "AIzaSyBWKDj-qfejNaK1ArNOgVZW0VIglkjP3v0",
+    authDomain: "covey-town-drawing-scores.firebaseapp.com",
+    projectId: "covey-town-drawing-scores",
+    storageBucket: "covey-town-drawing-scores.appspot.com",
+    messagingSenderId: "57019598391",
+    appId: "1:57019598391:web:7046cad384daa4810be1b2"
+  };
