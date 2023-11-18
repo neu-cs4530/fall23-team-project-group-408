3f08b856aedb95b25b71456a1c8e93f214839322
Dave Budhram <davebudhram18@gmail.com>
2023-11-18 18:03:28 -0500

    Added isPlayerOne and isPlayerTwo functions

--- frontend/src/classes/interactable/DrawThePerfectShape/DrawThePerfectShapeAreaController.ts
+++ frontend/src/classes/interactable/DrawThePerfectShape/DrawThePerfectShapeAreaController.ts
@@ -150,6 +150,20 @@ export default class DrawThePerfectShapeController extends GameAreaController<
   }
 
   /**
+   * Returns true if the current player is player one
+   */
+  get isPlayerOne(): boolean {
+    return this._model.game?.state.player1 === this._townController.ourPlayer.id || false;
+  }
+
+  /**
+   * Returns true if the current player is player two
+   */
+  get isPlayerTwo(): boolean {
+    return this._model.game?.state.player2 === this._townController.ourPlayer.id || false;
+  }
+
+  /**
    * Checks if the game has two players
    * @returns if there are two players currently in the game
    */
@@ -238,6 +252,23 @@ export default class DrawThePerfectShapeController extends GameAreaController<
     const { gameID } = await this._townController.sendInteractableCommand(this.id, {
       type: 'PickDifficulty',
       gameDifficulty: newDifficulty,
+      gameID: instanceID,
+    });
+    this._instanceID = gameID;
+  }
+
+  /**
+   * Sends a request to the server to start the game
+   *
+   * @throws An error if the server rejects the request start the game
+   */
+  public async startGame() {
+    const instanceID = this._instanceID;
+    if (!instanceID || this._model.game?.state.status !== 'IN_PROGRESS') {
+      throw new Error(NO_GAME_IN_PROGRESS_ERROR);
+    }
+    const { gameID } = await this._townController.sendInteractableCommand(this.id, {
+      type: 'StartGame',
       gameID: instanceID,
     });
     this._instanceID = gameID;
